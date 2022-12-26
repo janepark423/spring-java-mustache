27a95757746fa2433a1b7a2420b2a4d1fad3c2af
Jain <janepark423@gmail.com>
2022-12-27 01:17:28 +0900

    ..

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,16 @@ package com.example.firstproject.controller;
 
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm(){
@@ -17,6 +21,11 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form.toString());
+        
+        //1. Dto를 변환! Entity로
+        Article article = form.toEntity();
+        //2. DB에 저장 Repository에게 entity를 db안에 저장함
+        Article saved = articleRepository.save(article);
         return "";
     }
 }
