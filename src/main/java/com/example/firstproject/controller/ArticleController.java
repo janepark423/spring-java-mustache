adb8117925214994177037e3003501aaf1e887cb
jain <janepark@webiznet.com>
2022-12-27 19:10:55 +0900

    롬복 리팩터링

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,6 +7,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,12 +47,13 @@ public class ArticleController {
     }
 
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable  Long id){
+    public String show(@PathVariable  Long id, Model model){
         log.info("id = " + id);
         //TODO: 1번  ID로 데이터를 가져옴(리파지토리에서 가져옴)
         Article articleEntity = articleRepository.findById(id).orElse(null);
 
         //TODO: 2번 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
 
         //TODO: 3번  보여줄 페이지를 설정!
         return "";
