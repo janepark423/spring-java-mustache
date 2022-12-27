290b5dbfd8b71f87e83b72a49f4a230d0b4162b7
jain <janepark@webiznet.com>
2022-12-27 19:03:10 +0900

    롬복추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -4,12 +4,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 위한 골뱅이 sout 대신의 역할을 함
 public class ArticleController {
 
     @Autowired  //TODO: 스프링부트가 미리 생성해놓은 객체를 가져다가 자동(DI: 의존성 주입) 으로 연결(인터페이스)
@@ -26,15 +28,19 @@ public class ArticleController {
         
         //TODO: SOUT을 LOGGING으로 대체하자
         //System.out.println(form.toString());
+        log.info(form.toString());
         
         //TODO: 1. Dto를 변환! Entity로
         Article article = form.toEntity();
-        System.out.println(article.toString());
+
+        log.info(article.toString());
+       // System.out.println(article.toString());
         //TODO: 2. DB에 저장 Repository에게 entity를 db안에 저장함
         //TODO: Repository가 DB에 저장하는 기능을 함.
         Article saved = articleRepository.save(article);
         //FIXME: articleRepository를 saved로 반환해준다.
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
