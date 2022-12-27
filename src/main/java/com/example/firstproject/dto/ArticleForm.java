d2343ea2a44e377dd5d613a2b80a9ef016097b15
jain <janepark@webiznet.com>
2022-12-27 18:53:56 +0900

    롬복 리팩터링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,19 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+
+@AllArgsConstructor //생성자와 똑같은 역할
+@ToString //ToString과 똑같은 역할
 public class ArticleForm {
+
+    
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         //TODO: 엔티티 Article 객체를 반환해줌
