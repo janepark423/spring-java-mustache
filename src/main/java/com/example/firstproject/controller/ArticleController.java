94b3422f26eab5b52fac3a0de7ab28eb4d668a49
jain <janepark@webiznet.com>
2022-12-27 19:09:01 +0900

    orElse문

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -41,6 +42,18 @@ public class ArticleController {
         //FIXME: articleRepository를 saved로 반환해준다.
         log.info(saved.toString());
         //System.out.println(saved.toString());
+        return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable  Long id){
+        log.info("id = " + id);
+        //TODO: 1번  ID로 데이터를 가져옴(리파지토리에서 가져옴)
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //TODO: 2번 가져온 데이터를 모델에 등록
+
+        //TODO: 3번  보여줄 페이지를 설정!
         return "";
     }
 }
